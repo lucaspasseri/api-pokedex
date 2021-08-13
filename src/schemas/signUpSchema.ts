9567507eef7ce32d9e552733663be81428e05a1d
thiagoribb <thiribeiro142@gmail.com>
2021-08-13 19:20:26 -0300

    Preparando review

--- src/schemas/signUpSchema.ts
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const signUpSchema = joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().min(5).required(),
-    confirmPassword: joi.string().required().valid(joi.ref("password"))
-});
-
-export {signUpSchema}
